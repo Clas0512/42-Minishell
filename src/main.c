4be84261495f0adad3966306e2a471556646e208
Alperen Rüzgar Erbosnalı <97199520+Vencoheria@users.noreply.github.com>
2023-04-18 17:44:55 +0300

    Fixed Environ

--- src/main.c
+++ src/main.c
@@ -1,9 +1,4 @@
 #include "minishell.h"
-
-//EOF signal gives seg-fault, should fix it. 
-//It was working before but now its not and idk why lol, such a classic. 
-//I swear i didn't even touched it ;-; it might be about system call.
-//CHECK THIS WTF --->I think i fixed it, it was about the EOF signal, i changed it to 0 and it works now.
 //add cwdr etc. to global variable for the aesthetics.
 t_shell shell;
 
@@ -14,14 +9,13 @@ int	main(int ac, char *av[], char *env[])
 	char	*cwdr;
 	if (ft_strncmp(av[0], "./minishell", 11) == 0)
 	{
-		system("open -a Terminal ./minishell"); //fix the aesthetics, fixed the path for each computer
+		system("open -a iterm ./minishell"); //fix the aesthetics, fixed the path for each computer
 		exit(0);
 	}
 	(void)ac;
 	(void)av;
 	(void)env;
-	//environ changes the order idk why but gotta fix that
-	init_env();
+	init_env(); //order is changing in seperate terminal, check whh
 	init_collector(&shell.collector);
 	init_commander(&shell.commander);
 	signal(SIGINT, handle_signal);
@@ -41,17 +35,17 @@ int	main(int ac, char *av[], char *env[])
 		if (ft_strncmp(line, "exit", 4) == 0)
 			exit_minishell(linefornow);//works for now but should fix the aesthetics
 		else if (ft_strncmp(line, "echo", 4) == 0 || ft_strncmp(line, "ECHO", 4) == 0)
-			echo(ac, linefornow); //works for now
+			echo(ac, linefornow);
 		else if (ft_strncmp(line, "pwd", 3) == 0)
-			pwd();//works for now
+			pwd();
 		else if (ft_strncmp(line, "env", 3) == 0 || ft_strncmp(line, "ENV", 3) == 0)
-			print_env();//works for now
+			print_env();
  		else if (ft_strncmp(line, "unset", 5) == 0)
-			unset_env(linefornow);//works for now
+			unset_env(linefornow);
 		else if (ft_strncmp(line, "export", 6) == 0)
-			export_env(linefornow);//works for now
+			export_env(linefornow);
  		else if (ft_strncmp(line, "cd", 2) == 0)
-			change_dir(linefornow); //works for now
+			change_dir(linefornow);
 		else
 			printf("%s: command not found\n", line);
 		free(line);
